3129398050b0c6fa29aa331be6e1cc52dcf53997
Micael Dias <diasmicaelandre@gmail.com>
2020-12-10 22:11:53 +0000

    Add ANSI colors

---
+++ src/colors.h
@@ -0,0 +1,15 @@
+/*
+ * Created by Micael Dias on 10/12/20.
+ */
+
+#ifndef _COLORS_H_
+#define _COLORS_H_
+
+#define RED(string)      "\x1b[31m" string "\x1b[0m"
+#define GREEN(string)    "\x1b[32m" string "\x1b[0m"
+#define YELLOW(string)   "\x1b[33m" string "\x1b[0m"
+#define BLUE(string)     "\x1b[34m" string "\x1b[0m"
+#define MAGENTA(string)  "\x1b[35m" string "\x1b[0m"
+#define CYAN(string)     "\x1b[36m" string "\x1b[0m"
+
+#endif //_COLORS_H_
