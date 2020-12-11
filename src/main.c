99d9f78bb26d02f47eacd91288af6c4e4e34c7fd
Micael Dias <diasmicaelandre@gmail.com>
2020-12-11 22:31:07 +0000

    Testing not_married_table

--- src/main.c
+++ src/main.c
@@ -3,23 +3,37 @@
 #include "strs.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
+	s_table* not_married_table;
+	char* not_married_str;
+	int not_married_len;
+
 	fprintf(stdout, "%s", H_STRS_MENU);
 	fprintf(stdout, "%s", H_STRS_SAVE_MENU);
 
-	int not_married_str_len = 0;
-	char* not_married_str = h_util_file_read("../assets/table_not_married.csv", not_married_str_len);
+	not_married_str = h_util_file_read("../assets/table_not_married.csv");
 	if (not_married_str == NULL)
 		return 1;
 
-	s_table* not_married_table = h_table_alloc(not_married_str_len);
+	not_married_len = h_util_get_lines_from_str(not_married_str);
+	not_married_table = h_table_alloc(not_married_len);
 	if (not_married_table == NULL)
 		return 2;
 
-	// Print the value
-	//h_table_init_from_str(not_married_table, not_married_str);
+
+	h_table_init_from_str(not_married_table, not_married_len, not_married_str);
+
+
+	for (int i = 0; i < not_married_len; i++) {
+		fprintf(stdout, "%.2f\n", not_married_table[i].monthly_pay);
+	}
+
+
+	free(not_married_table);
+
 
 	return 0;
 }
