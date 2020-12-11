56504dbd645ab49cc0d8b140c9c1b65000326a35
Micael Dias <diasmicaelandre@gmail.com>
2020-12-11 22:23:55 +0000

    Improve file read function like variable naming and implement function to count the number of lines on a file

--- src/util.c
+++ src/util.c
@@ -14,25 +14,30 @@
  * @param len Save into a variable the len of the file
  * @return char*
  */
-char* h_util_file_read(const char* path, long len)
+char* h_util_file_read(const char* path)
 {
-	FILE* fp = fopen(path, "r");
+	FILE* fp;
+	unsigned long total_bytes;
+	unsigned long read_bytes;
+	char* str;
+
+	fp = fopen(path, "r");
 	if (fp == NULL)
 		return NULL;
 
 	fseek(fp, 0L, SEEK_END);
-	len = ftell(fp) + 1;
+	total_bytes = ftell(fp) + 1;
 	fseek(fp, 0L, SEEK_SET);
 
-	char* str = malloc(len);
+	str = malloc(total_bytes);
 	if (str == NULL)
 	{
 		fclose(fp);
 		return NULL;
 	}
 
-	size_t chucks = fread(str, sizeof(char), len - 1, fp);
-	if (chucks < len - 1)
+	read_bytes = fread(str, sizeof(char), total_bytes - 1, fp);
+	if (read_bytes < total_bytes - 1)
 	{
 		free(str);
 		fclose(fp);
@@ -40,6 +45,22 @@ char* h_util_file_read(const char* path, long len)
 	}
 
 	fclose(fp);
-	str[len - 1] = '\0';
+	str[total_bytes - 1] = '\0';
 	return str;
+}
+
+int h_util_get_lines_from_str(const char* str) {
+	int i;
+	int lines;
+
+	i = 0;
+	lines = 0;
+	while (str[i] != '\0') {
+		if (str[i] == '\n') {
+			lines++;
+		}
+		i++;
+	}
+
+	return lines;
 }
--- src/util.h
+++ src/util.h
@@ -5,6 +5,7 @@
 #ifndef _UTIL_H
 #define _UTIL_H
 
-char* h_util_file_read(const char* path, long len);
+char* h_util_file_read(const char* path);
+int h_util_get_lines_from_str(const char* str);
 
 #endif //_UTIL_H
