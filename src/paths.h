7ba0f76ed6ececdeef92b22769d033ba66b48ba6
Micael Dias <diasmicaelandre@gmail.com>
2020-12-18 22:06:08 +0000

    Delete bin paths they are no longer need

--- src/paths.h
+++ src/paths.h
@@ -6,12 +6,7 @@
 #define _PATHS_H_
 
 #define H_PATH_SINGLE "../data/table_not_married.csv"
-#define H_PATH_SINGLE_BIN "../data/table_not_married.bin"
-
 #define H_PATH_UNIQUE_HOLDER "../data/table_married_unique_holder.csv"
-#define H_PATH_UNIQUE_HOLDER_BIN "../data/table_married_unique_holder.bin"
-
 #define H_PATH_TWO_HOLDERS "../data/table_married_two_holders.csv"
-#define H_PATH_TWO_HOLDERS_BIN "../data/table_married_two_holders.bin"
 
 #endif //_PATHS_H_
