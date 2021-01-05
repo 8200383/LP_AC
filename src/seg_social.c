584ab7e4782dcfccc8e20873fb4fc5514d4dc14b
Hugo Carvalho <72693162+HCarvalhoGit@users.noreply.github.com>
2021-01-05 18:47:33 +0000

    Update seg_social.c
    Substituir os ";" por apenas ","

--- src/seg_social.c
+++ src/seg_social.c
@@ -47,7 +47,7 @@ s_error* h_seg_social_parse(s_arr_seg_social* array, char* str)
 		if (offset == -1 && isalnum(str[i]))
 			offset = i;
 
-		if (offset != -1 && str[i] == ';')
+		if (offset != -1 && str[i] == ',')
 		{
 			str[i] = '\0';
 			if (str[i - 1] == '%')
@@ -161,7 +161,7 @@ s_error* h_seg_social_write(s_arr_seg_social* array, const char* path)
 
 	for (i = 0; i <= array->used; i++)
 	{
-		fprintf(fp, "%c;%.2f%%;%.2f%%;\n",
+		fprintf(fp, "%c,%.2f%%,%.2f%%,\n",
 			array->data[i].criteria,
 			array->data[i].employer,
 			array->data[i].employee);
