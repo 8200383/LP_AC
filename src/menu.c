255ed3699e5f60c71a7056e4edc658fc1ac3b913
Hugo Carvalho <72693162+HCarvalhoGit@users.noreply.github.com>
2021-01-06 22:45:00 +0000

    Update menu.c

--- src/menu.c
+++ src/menu.c
@@ -14,84 +14,88 @@ void h_menu_irs(
 {
 	unsigned int op;
 
-	fprintf(stdout, H_STRS_IRS_MENU);
-	fprintf(stdout, GREEN("%s"), H_STRS_PROMPT);
-	scanf(" %u", &op);
+	do {
+		fprintf(stdout, H_STRS_IRS_MENU);
+		fprintf(stdout, GREEN("%s"), H_STRS_PROMPT);
+		scanf(" %u", &op);
 
-	switch (op)
-	{
-	case 1:
-		h_irs_print(single_table);
-		break;
-	case 2:
-		h_irs_edit(single_table, h_util_get_int(0, 100, H_STRS_EDIT));
-		break;
-	case 3:
-		h_irs_add(single_table);
-		break;
-	case 4:
-		h_irs_delete(single_table, h_util_get_int(0, 100, H_STRS_DELETE));
-		break;
-	case 5:
-		h_irs_print(unique_holder_table);
-		break;
-	case 6:
-		h_irs_edit(unique_holder_table, h_util_get_int(0, 100, H_STRS_EDIT));
-		break;
-	case 7:
-		h_irs_add(unique_holder_table);
-		break;
-	case 8:
-		h_irs_delete(unique_holder_table, h_util_get_int(0, 100, H_STRS_DELETE));
-		break;
-	case 9:
-		h_irs_print(two_holders_table);
-		break;
-	case 10:
-		h_irs_edit(two_holders_table, h_util_get_int(0, 100, H_STRS_EDIT));
-		break;
-	case 11:
-		h_irs_add(two_holders_table);
-		break;
-	case 12:
-		h_irs_delete(two_holders_table, h_util_get_int(0, 100, H_STRS_DELETE));
-		break;
-	case 0:
-		break;
-	default:
-		fprintf(stdout, RED("%s"), H_STRS_INVALID_IMPUT);
-		break;
-	}
+		switch (op)
+		{
+		case 1:
+			h_irs_print(single_table);
+			break;
+		case 2:
+			h_irs_edit(single_table, h_util_get_int(0, 100, H_STRS_EDIT));
+			break;
+		case 3:
+			h_irs_add(single_table);
+			break;
+		case 4:
+			h_irs_delete(single_table, h_util_get_int(0, 100, H_STRS_DELETE));
+			break;
+		case 5:
+			h_irs_print(unique_holder_table);
+			break;
+		case 6:
+			h_irs_edit(unique_holder_table, h_util_get_int(0, 100, H_STRS_EDIT));
+			break;
+		case 7:
+			h_irs_add(unique_holder_table);
+			break;
+		case 8:
+			h_irs_delete(unique_holder_table, h_util_get_int(0, 100, H_STRS_DELETE));
+			break;
+		case 9:
+			h_irs_print(two_holders_table);
+			break;
+		case 10:
+			h_irs_edit(two_holders_table, h_util_get_int(0, 100, H_STRS_EDIT));
+			break;
+		case 11:
+			h_irs_add(two_holders_table);
+			break;
+		case 12:
+			h_irs_delete(two_holders_table, h_util_get_int(0, 100, H_STRS_DELETE));
+			break;
+		case 0:
+			break;
+		default:
+			fprintf(stdout, RED("%s"), H_STRS_INVALID_IMPUT);
+			break;
+		}
+	} while (op != 0);
 }
 
 void h_menu_seg_social(s_arr_seg_social* seg_social_table)
 {
 	unsigned int op;
 
-	fprintf(stdout, H_STRS_SEG_SOCIAL_MENU);
-	fprintf(stdout, GREEN("%s"), H_STRS_PROMPT);
-	scanf(" %u", &op);
+	do {
+		fprintf(stdout, H_STRS_SEG_SOCIAL_MENU);
+		fprintf(stdout, GREEN("%s"), H_STRS_PROMPT);
+		scanf(" %u", &op);
 
-	switch (op)
-	{
-	case 1:
-		h_seg_social_print(seg_social_table);
-		break;
-	case 2:
-		h_seg_social_add(seg_social_table);
-		break;
-	case 3:
-		h_seg_social_delete(seg_social_table);
-		break;
-	case 4:
-		h_seg_social_edit(seg_social_table);
-		break;
-	case 0:
-		break;
-	default:
-		fprintf(stdout, RED("%s"), H_STRS_INVALID_IMPUT);
-		break;
-	}
+		switch (op)
+		{
+		case 1:
+			h_seg_social_print(seg_social_table);
+			break;
+		case 2:
+			h_seg_social_add(seg_social_table);
+			break;
+		case 3:
+			h_seg_social_delete(seg_social_table);
+			break;
+		case 4:
+			h_seg_social_edit(seg_social_table);
+			break;
+		case 0:
+			break;
+		default:
+			fprintf(stdout, RED("%s"), H_STRS_INVALID_IMPUT);
+			break;
+		}
+	} while (op != 0);
 }
 
 void h_menu_processing(
