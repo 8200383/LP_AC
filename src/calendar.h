83114577d6e2932599bcc6faeac8bfb490277007
Micael Dias <diasmicaelandre@gmail.com>
2021-01-10 01:06:14 +0000

    document calendar

--- src/calendar.h
+++ src/calendar.h
@@ -21,6 +21,9 @@ typedef struct
 	int year;
 } s_date;
 
+/**
+ * @enum e_month Represents the Months from 1 to 12
+ */
 typedef enum
 {
 	JAN = 1,
@@ -37,13 +40,54 @@ typedef enum
 	DEC = 12
 } e_month;
 
+/**
+ * Gets a date from stdin
+ * @param date The struct to fill
+ * @param msg The message to display
+ */
 void h_calendar_get_date(s_date* date, const char* msg);
-int h_calendar_days_in_month(e_month month);
-int h_calendar_check_date(int day, int month, int year);
-int h_calendar_leap_year(int year);
-char* h_calendar_str_from_month(e_month month);
-struct tm* h_calendar_init_local_time();
-int h_calendar_get_year(struct tm* local_time);
 
+/**
+ * Gets the number of days in a month
+ * @param month The month to calculate
+ * @return int Returns the nummber of days
+ */
+int h_calendar_days_in_month(e_month month);
+
+/**
+ * Checks if a date is valid
+ * @param day The day to validate depends on the onth
+ * @param month The month to validate from 1 to 12
+ * @param year The year to validate, from 1000 to 9999
+ * @return int Return 0 if fail, otherwise returns 1
+ */
+int h_calendar_check_date(int day, int month, int year);
+
+/**
+ * Checks if it is a leap year
+ * @param year The year to validate
+ * @return int Returns 1 if is a leap year, otherwise returns 0
+ */
+int h_calendar_leap_year(int year);
+
+/**
+ * Converts e_month enumeration to string
+ * @param month The month to convert
+ * @return char* Returns the month into a string
+ */
+char* h_calendar_str_from_month(e_month month);
+
+/**
+ * Initializes system time
+ * @return Returns a broken-down time struct (ISO C)
+ */
+struct tm* h_calendar_init_local_time();
+
+/**
+ * Gets the current year
+ * @param local_time The system time struct
+ * @return int Returns the current year
+ */
+int h_calendar_get_year(struct tm* local_time);
 
 #endif //CALENDAR_H
