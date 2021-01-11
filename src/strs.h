906822a1999ff3e3a16707c17f0c22998437740e
Micael Dias <diasmicaelandre@gmail.com>
2021-01-11 09:14:23 +0000

    delete broken option since we delete the month when we go back

--- src/strs.h
+++ src/strs.h
@@ -96,8 +96,7 @@
 "*  7. Exportar mês para CSV     *\n" \
 "*  8. Guardar                   *\n" \
 "*  9. Importar mês              *\n" \
-"* 10. Desfazer todas alterações *\n" \
-"* 0. Voltar ao menu anterior    *\n" \
+"*  0. Voltar ao menu anterior   *\n" \
 "*                               *\n" \
 "*********************************\n")
 
