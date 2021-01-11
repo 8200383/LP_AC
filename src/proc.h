50cc558ec673173f80bdb88f7e1401a1bae6dcdc
Micael Dias <diasmicaelandre@gmail.com>
2021-01-11 22:17:43 +0000

    document proc consts

--- src/proc.h
+++ src/proc.h
@@ -19,11 +19,11 @@
 #include "employees.h"
 
 #define MAX_FILENAME 256
-/** @const BONUS_BASE */
+/** @const BONUS_BASE Base bonuses awarded */
 #define BONUS_BASE 1.0f
-/** @const BONUS_20_DAYS */
+/** @const BONUS_20_DAYS Bonuses awarded for 20 working days */
 #define BONUS_20_DAYS 1.05f
-/** @const BONUS_17_DAYS */
+/** @const BONUS_17_DAYS Bonuses awarded for 17 working days */
 #define BONUS_17_DAYS 1.02f
 
 /**
@@ -32,19 +32,19 @@
  */
 typedef struct
 {
-	int cod_employee; /**< Reprents the employee code */
-	int full_days; /**< Represents the number of full days worked */
-	int half_days; /**< Represents the number of half days worked */
-	int weekend_days; /**< Represents number of the weekends worked */
-	int absent_days; /**< Represents the number of days missed */
-	float gross_pay; /**< Salary before taxes */
-	float net_pay; /**< Salary afeter taxes */
-	float bonus; /**< Bonus awarded */
-	float food_allowance; /**< Food allowance calculated */
-	float irs_retention; /**< IRS retention percentage */
-	float iss_retention_employer; /**< ISS retention percentage for employer */
-	float iss_retention_employee; /**< ISS retention percentage for employee */
-	float employer_charge; /**< Total employer charge */
+    int cod_employee; /**< Reprents the employee code */
+    int full_days; /**< Represents the number of full days worked */
+    int half_days; /**< Represents the number of half days worked */
+    int weekend_days; /**< Represents number of the weekends worked */
+    int absent_days; /**< Represents the number of days missed */
+    float gross_pay; /**< Salary before taxes */
+    float net_pay; /**< Salary afeter taxes */
+    float bonus; /**< Bonus awarded */
+    float food_allowance; /**< Food allowance calculated */
+    float irs_retention; /**< IRS retention percentage */
+    float iss_retention_employer; /**< ISS retention percentage for employer */
+    float iss_retention_employee; /**< ISS retention percentage for employee */
+    float employer_charge; /**< Total employer charge */
 } s_details;
 
 /**
@@ -53,10 +53,10 @@ typedef struct
  */
 typedef struct
 {
-	s_details* details; /**< Array of details **/
-	e_month month; /**< Represent the month of the spreadsheet **/
-	int used; /**< Tracks used capacity of the array **/
-	int max_capacity; /**< Tracks max capacity of the array **/
+    s_details *details; /**< Array of details **/
+    e_month month; /**< Represent the month of the spreadsheet **/
+    int used; /**< Tracks used capacity of the array **/
+    int max_capacity; /**< Tracks max capacity of the array **/
 } s_spreadsheet;
 
 /**
@@ -64,7 +64,7 @@ typedef struct
  * containing the month (enum) and details (s_details) for the month
  * @return s_spreadsheet Returns a ptr to a malloced struct of s_spreadsheet returns NULL if failure
  */
-s_spreadsheet* h_proc_alloc(int initial_capacity);
+s_spreadsheet *h_proc_alloc(int initial_capacity);
 
 /**
  * Opens a binary file in read mode onto a spreadsheet
@@ -72,56 +72,56 @@ s_spreadsheet* h_proc_alloc(int initial_capacity);
  * @param month Month to import
  * @return s_spreadsheet Returns a ptr to a malloced struct of s_spreadsheet returns NULL if failure
  */
-s_spreadsheet* h_proc_read(const char* filename, e_month month);
+s_spreadsheet *h_proc_read(const char *filename, e_month month);
 
 /**
  * Loads a spreadsheet into memory by asking a month to import
  * @return s_spreadsheet Returns a ptr to a malloced struct of s_spreadsheet returns NULL if failure
  */
-s_spreadsheet* h_proc_import();
+s_spreadsheet *h_proc_import();
 
 /**
  * Writes into a binary file a spreadsheet
  * @param spreadsheet The spreadsheet referring to a month
  */
-void h_proc_write(s_spreadsheet* spreadsheet, const char* path);
+void h_proc_write(s_spreadsheet *spreadsheet, const char *path);
 
 /**
  * Add details to an existing month
  * @param spreadsheet Ptr to a spreadsheet
  * @param arr_employees An array of employees
  */
-void h_proc_add(s_spreadsheet* spreadsheet, s_arr_employees* arr_employees);
+void h_proc_add(s_spreadsheet *spreadsheet, s_arr_employees *arr_employees);
 
 /**
  * Edit details from an existing month
  * @param spreadsheet Ptr to a spreadsheet
  */
-void h_proc_edit(s_spreadsheet* spreadsheet);
+void h_proc_edit(s_spreadsheet *spreadsheet);
 
 /**
  * Prints a spreadsheet
  * @param spreadsheet Ptr of spreadsheet
  */
-void h_proc_print(s_spreadsheet* spreadsheet);
+void h_proc_print(s_spreadsheet *spreadsheet);
 
 /**
  * Deletes details in a spreadsheet
  * @param spreadsheet Ptr of spreadsheet
  */
-void h_proc_delete(s_spreadsheet* spreadsheet);
+void h_proc_delete(s_spreadsheet *spreadsheet);
 
 /**
  * Free a spreadsheet
  * @param spreadsheet Ptr of spreadsheet
  */
-void h_proc_free(s_spreadsheet* spreadsheet);
+void h_proc_free(s_spreadsheet *spreadsheet);
 
 /**
  * Exports a spreadsheet to CSV
  * @param spreadsheet Ptr of spreadsheet
  */
-void h_proc_export_csv(s_spreadsheet* spreadsheet);
+void h_proc_export_csv(s_spreadsheet *spreadsheet);
 
 /**
  * Generate a filename from a given month and an extension
@@ -129,7 +129,7 @@ void h_proc_export_csv(s_spreadsheet* spreadsheet);
  * @param extension The file extension
  * @return char* Return the generated filename
  */
-char* h_proc_generate_filename(e_month month, const char* extension);
+char *h_proc_generate_filename(e_month month, const char *extension);
 
 /**
  * Perform the salary calculations
@@ -141,12 +141,12 @@ char* h_proc_generate_filename(e_month month, const char* extension);
  * @param employees_array The employees array
  */
 void h_proc_perform(
-	s_spreadsheet* spreadsheet,
-	s_arr_irs* single_array,
-	s_arr_irs* unique_holder_array,
-	s_arr_irs* two_holders_array,
-	s_arr_iss* iss_array,
-	s_arr_employees* employees_array
+        s_spreadsheet *spreadsheet,
+        s_arr_irs *single_array,
+        s_arr_irs *unique_holder_array,
+        s_arr_irs *two_holders_array,
+        s_arr_iss *iss_array,
+        s_arr_employees *employees_array
 );
 
 /**
@@ -156,13 +156,13 @@ void h_proc_perform(
  * @param raw_salary The salary before retentions
  * @return Returns the retention percentage
  */
-float h_proc_get_retention_percentage(s_arr_irs* irs_array, int dependents, float raw_salary);
+float h_proc_get_retention_percentage(s_arr_irs *irs_array, int dependents, float raw_salary);
 
 /**
  * Writes into a file path a spreadsheet
  * @param spreadsheet A spreadsheet referring a month
  * @param path The file path
  */
-void h_proc_write(s_spreadsheet* spreadsheet, const char* path);
+void h_proc_write(s_spreadsheet *spreadsheet, const char *path);
 
 #endif //PROC_H
