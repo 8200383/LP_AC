0a1025aeb90f8a7540ca577ab6fbf32fef942f76
Micael Dias <diasmicaelandre@gmail.com>
2021-01-11 21:39:25 +0000

    use smaller path name

--- src/paths.h
+++ src/paths.h
@@ -12,7 +12,7 @@
 #define H_PATH_UNIQUE_HOLDER "../data/table_married_unique_holder.csv"
 #define H_PATH_TWO_HOLDERS "../data/table_married_two_holders.csv"
 
-#define H_PATH_SEG_SOCIAL "../data/table_seg_social.csv"
+#define H_PATH_ISS "../data/table_seg_social.csv"
 #define H_PATH_EMPLOYEES "../data/table_employees.csv"
 
 #endif //PATHS_H
