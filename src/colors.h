e52be402772c8ab3fa55476da1e4bac2ba7e5c65
Micael Dias <diasmicaelandre@gmail.com>
2021-01-12 00:45:29 +0000

    remove magenta

--- src/colors.h
+++ src/colors.h
@@ -12,7 +12,6 @@
 #define GREEN(string)    "\x1b[32m" string "\x1b[0m"
 #define YELLOW(string)   "\x1b[33m" string "\x1b[0m"
 #define BLUE(string)     "\x1b[34m" string "\x1b[0m"
-#define MAGENTA(string)  "\x1b[35m" string "\x1b[0m"
 #define CYAN(string)     "\x1b[36m" string "\x1b[0m"
 
 #endif //COLORS_H
